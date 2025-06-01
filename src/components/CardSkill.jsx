0e76a0fa1d8c05114971a8ed875dd2360ea542c1
Case1490 <renzoramos414@gmail.com>
2025-06-01 01:55:15 -0500

    mitad

--- src/components/CardSkill.jsx
+++ src/components/CardSkill.jsx
@@ -1,13 +1,15 @@
 import ReactIcon from "../assets/habilidades/react.png";
 
-const CardSkill = () => {
+const CardSkill = ({ image, title, percentage }) => {
   return (
     <div className=" font-bold text-white p-3 space-y-3 border-2 border-[var(--GrayMain)] rounded-2xl w-[250px] flex flex-col items-center">
       <div className="w-[120px]">
-        <img src={ReactIcon} alt="Icon" className="w-full" />
+        <img src={image} alt="Icon" className="w-full" />
       </div>
-      <h1 className="">Figma</h1>
-      <p className="bg-[var(--OrangeMain)] w-[95%] rounded-full m-auto">99%</p>
+      <h1 className="">{title}</h1>
+      <p className="bg-[var(--OrangeMain)] w-[95%] rounded-full m-auto">
+        {percentage}%
+      </p>
     </div>
   );
 };
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,5 +1,82 @@
 import CardSkill from "../components/CardSkill";
 
+// IMPORT IMAGES ICONS
+import Angular from "../assets/habilidades/angular.png";
+import Bootstrap from "../assets/habilidades/bootstrap.png";
+import Figma from "../assets/habilidades/figma.png";
+import Firebase from "../assets/habilidades/firebase.png";
+import Git from "../assets/habilidades/git.png";
+import MongoDB from "../assets/habilidades/mongodb.png";
+import MySQL from "../assets/habilidades/mysql.png";
+import Node from "../assets/habilidades/node.png";
+import Python from "../assets/habilidades/python.png";
+import React from "../assets/habilidades/react.png";
+import Tailwind from "../assets/habilidades/tailwind.png";
+import Vue from "../assets/habilidades/vue.png";
+
+const dataSkills = [
+  {
+    image: Angular,
+    title: "Angular",
+    percentage: "60",
+  },
+  {
+    image: Bootstrap,
+    title: "Bootstrap",
+    percentage: "99",
+  },
+  {
+    image: Figma,
+    title: "Figma",
+    percentage: "99",
+  },
+  {
+    image: Firebase,
+    title: "Firebase",
+    percentage: "99",
+  },
+  {
+    image: Git,
+    title: "Git",
+    percentage: "99",
+  },
+  {
+    image: MongoDB,
+    title: "MongoDB",
+    percentage: "99",
+  },
+  {
+    image: MySQL,
+    title: "MySQL",
+    percentage: "99",
+  },
+  {
+    image: Node,
+    title: "Node.js",
+    percentage: "99",
+  },
+  {
+    image: Python,
+    title: "Python",
+    percentage: "99",
+  },
+  {
+    image: React,
+    title: "React",
+    percentage: "99",
+  },
+  {
+    image: Tailwind,
+    title: "Tailwind",
+    percentage: "99",
+  },
+  {
+    image: Vue,
+    title: "Vue.js",
+    percentage: "99",
+  },
+];
+
 const Skills = () => {
   return (
     <div>
@@ -8,12 +85,15 @@ const Skills = () => {
           Habilidades
         </h1>
 
-        <div className="flex space-x-4 justify-center">
-          <CardSkill />
-          <CardSkill />
-          <CardSkill />
-          <CardSkill />
-          <CardSkill />
+        <div className="grid grid-cols-4 gap-y-8">
+          {dataSkills.map((skill, index) => (
+            <CardSkill
+              key={index}
+              image={skill.image}
+              title={skill.title}
+              percentage={skill.percentage}
+            />
+          ))}
         </div>
       </div>
     </div>
