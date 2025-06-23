9543a2684daa28092f4319c9aacee4968eb56a27
Case1490 <renzoramos414@gmail.com>
2025-06-23 17:57:11 -0500

    terminado 5/7

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,82 +1,3 @@
-import CardSkill from "../components/CardSkill";
-
-// IMPORT IMAGES ICONS
-import Angular from "../assets/habilidades/angular.png";
-import Bootstrap from "../assets/habilidades/bootstrap.png";
-import Figma from "../assets/habilidades/figma.png";
-import Firebase from "../assets/habilidades/firebase.png";
-import Git from "../assets/habilidades/git.png";
-import MongoDB from "../assets/habilidades/mongodb.png";
-import MySQL from "../assets/habilidades/mysql.png";
-import Node from "../assets/habilidades/node.png";
-import Python from "../assets/habilidades/python.png";
-import React from "../assets/habilidades/react.png";
-import Tailwind from "../assets/habilidades/tailwind.png";
-import Vue from "../assets/habilidades/vue.png";
-
-const dataSkills = [
-  {
-    image: Angular,
-    title: "Angular",
-    percentage: "60",
-  },
-  {
-    image: Bootstrap,
-    title: "Bootstrap",
-    percentage: "90",
-  },
-  {
-    image: Figma,
-    title: "Figma",
-    percentage: "75",
-  },
-  {
-    image: Firebase,
-    title: "Firebase",
-    percentage: "90",
-  },
-  {
-    image: Git,
-    title: "Git",
-    percentage: "80",
-  },
-  {
-    image: MongoDB,
-    title: "MongoDB",
-    percentage: "85",
-  },
-  {
-    image: MySQL,
-    title: "MySQL",
-    percentage: "70",
-  },
-  {
-    image: Node,
-    title: "Node.js",
-    percentage: "85",
-  },
-  {
-    image: Python,
-    title: "Python",
-    percentage: "80",
-  },
-  {
-    image: React,
-    title: "React",
-    percentage: "86",
-  },
-  {
-    image: Tailwind,
-    title: "Tailwind",
-    percentage: "80",
-  },
-  {
-    image: Vue,
-    title: "Vue.js",
-    percentage: "75",
-  },
-];
-
 const Skills = () => {
   return (
     <div>
@@ -90,16 +11,19 @@ const Skills = () => {
             formación, práctica y constante aprendizaje.
           </p>
         </div>
-
-        <div className="grid grid-cols-4 gap-y-8">
-          {dataSkills.map((skill, index) => (
-            <CardSkill
-              key={index}
-              image={skill.image}
-              title={skill.title}
-              percentage={skill.percentage}
-            />
-          ))}
+        <div className="container">
+          <div className="tarjeta" id="angular"></div>
+          <div className="tarjeta" id="bootstrap"></div>
+          <div className="tarjeta" id="figma"></div>
+          <div className="tarjeta" id="firebase"></div>
+          <div className="tarjeta" id="git"></div>
+          <div className="tarjeta" id="mongodb"></div>
+          <div className="tarjeta" id="mysql"></div>
+          <div className="tarjeta" id="node"></div>
+          <div className="tarjeta" id="python"></div>
+          <div className="tarjeta" id="react"></div>
+          <div className="tarjeta" id="tailwind"></div>
+          <div className="tarjeta" id="vue"></div>
         </div>
       </div>
     </div>
